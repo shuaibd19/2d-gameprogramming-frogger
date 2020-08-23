fde43c46ec37cb4b4fca2a2d70acf15059bb2ea3
Ahmad Dorani <Ahmad.Dorani@mds.torrens.edu.au>
2020-08-24 03:19:08 +1000

    Added UI
    Score now displays

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    private GameManager myGameManager; //A reference to the GameManager in the scene.
+    public Text scoreTxt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject theManager = GameObject.Find("GameManager");
+        myGameManager = theManager.GetComponent<GameManager>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreTxt.text = "Score: " + myGameManager.getCurrentPoints().ToString();
+    }
+}
