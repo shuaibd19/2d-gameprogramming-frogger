85341f41098833a3ee93b49775fb466b0ffbe9f3
Ahmad Dorani <Ahmad.Dorani@mds.torrens.edu.au>
2020-08-23 21:53:44 +1000

    Added logic to vehicle
    the vehicle now moves along the x axis at random speeds and direction

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -14,7 +14,7 @@ public class Player : MonoBehaviour
     public int playerLivesRemaining; //PLayers actual lives remaining.
    
     public bool playerIsAlive = true; //Is the player currently alive?
-    public bool playerCanMove = false; //Can the player currently move?
+    public bool playerCanMove = true; //Can the player currently move?
 
     public float playerSpeed = 5f;
 
@@ -35,25 +35,34 @@ public class Player : MonoBehaviour
         //adding movement functionality to player using arrow keys
         //player's movement is not continous and only snaps into a single unit in the grid
 
+        //------------------TODO------------------------
+        //MAKE SURE THE PLAYER DOES NOT EXCEED BOUNDS OF WORLD SPACE
         //up
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        if (playerIsAlive)
         {
-            rBody.MovePosition(rBody.position + Vector2.up);
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                rBody.MovePosition(rBody.position + Vector2.up);
+            }
+            //down
+            else if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                rBody.MovePosition(rBody.position + Vector2.down);
+            }
+            //left
+            else if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                rBody.MovePosition(rBody.position + Vector2.left);
+            }
+            //right
+            else if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                rBody.MovePosition(rBody.position + Vector2.right);
+            }
         }
-        //down
-        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        else
         {
-            rBody.MovePosition(rBody.position + Vector2.down);
-        }
-        //left
-        else if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            rBody.MovePosition(rBody.position + Vector2.left);
-        }
-        //right
-        else if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            rBody.MovePosition(rBody.position + Vector2.right);
+            playerCanMove = false;
         }
 
     }
---
+++ Assets/Scripts/Vehicle.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This script must be utlised as the core component on the 'vehicle' obstacle in the frogger game.
+/// </summary>
+public class Vehicle : MonoBehaviour
+{
+    /// <summary>
+    /// -1 = left, 1 = right
+    /// </summary>
+    public Vector2 startingPosition; //This variable is to be used to indicate where on the map the vehicle starts (or spawns)
+    public Vector2 endPosition; //This variablle is to be used to indicate the final destination of the vehicle.
+
+    Rigidbody2D rBody;
+    Vector2 fwd;
+    float maxSpeed = 10f; //setting the upper limit of vehicle spawn speed when randomising
+    float minSpeed = 2.5f; //setting the lower limit of vehicle spawn speed when randomising
+    float speed; //This variable is to be used to control the speed of the vehicle.
+
+    int moveDirection; //This variabe is to be used to indicate the direction the vehicle is moving in.
+    float dirMax = 2f;
+    float dirMin = -2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rBody = GetComponent<Rigidbody2D>();
+        //randomising speed at spawn time
+        speed = Random.Range(maxSpeed, minSpeed);
+        //randomising movedirection of vehicle at spawn time
+        if (Random.Range(dirMax, dirMin) > 0)
+        {
+            moveDirection = 1;
+        }
+        else
+        {
+            moveDirection = -1;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        fwd = new Vector2(transform.right.x, transform.right.y);
+        rBody.MovePosition(rBody.position + fwd * Time.fixedDeltaTime * speed * moveDirection);
+    }
+
+}
